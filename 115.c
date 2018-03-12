f7bc9f47b1dadeee60e24e9d44a030081879300e
karthikeyan2 <35687514+karthikeyan2@users.noreply.github.com>
2018-03-12 16:21:22 +0530

    115.c

---
+++ 115.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include <conio.h>
+int main(void)
+{
+int j,N,K,i,a[100],b[100],temp;
+scanf("%d %d",&N,&K);
+for(i=0;i<N;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<N;i++)
+{
+for(j=i+1;j<N;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+printrf("%d",a[k-1]);
+return 0;
+}
